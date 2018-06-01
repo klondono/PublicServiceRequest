5ecc285d76742d1f63536e3ffb5b3afb6021afc0
Kerwin Londono <klondono1016@gmail.com>
2018-06-01 14:35:31 -0400

    Update Encryptor.cs

--- PublicServiceRequest/Utilities/Encryptor.cs
+++ PublicServiceRequest/Utilities/Encryptor.cs
@@ -78,7 +78,7 @@ namespace CustomerService_PSR.Utilities
 
 
 
-        static string _Pwd = "isstaff"; //Be careful storing this in code unless it’s secured and not distributed
+        static string _Pwd = "someValue"; //Be careful storing this in code unless it’s secured and not distributed
         static byte[] _Salt = new byte[] { 0x45, 0xF1, 0x61, 0x6e, 0x20, 0x00, 0x65, 0x64, 0x76, 0x65, 0x64, 0x03, 0x76 };
     }
 }
